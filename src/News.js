5fa334bb0e2237ceeeb78d9165192d584240adee
Sai Vijay Nagarur <78963023+VIJAYRUR@users.noreply.github.com>
2022-10-04 14:57:31 +0530

    Update News.js

--- src/News.js
+++ src/News.js
@@ -50,15 +50,15 @@ const News = (props)=>{
                 <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
                 {loading && <Spinner />}
                 <InfiniteScroll
-                    dataLength={articles.length}
+                    dataLength={articles?articles.length:0}
                     next={fetchMoreData}
-                    hasMore={articles.length !== totalResults}
+                    hasMore={articls:articles.length !== totalResults:false}
                     loader={<Spinner/>}
                 > 
                     <div className="container">
                          
                     <div className="row">
-                        {articles.map((element) => {
+                        {articles?.articles.map((element) => {
                             return <div className="col-md-4" key={element.url}>
                                 <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                             </div>
